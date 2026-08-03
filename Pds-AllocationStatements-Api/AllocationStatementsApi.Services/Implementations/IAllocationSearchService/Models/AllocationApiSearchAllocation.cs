41ad362402c848c0e86aaac21b6a5476edc419f9
AD\DMAKEJEV <dmitri.makejev@education.gov.uk>
2026-08-03 14:51:09 +0100

    refactor: Reverted required modifiers

--- Pds-AllocationStatements-Api/AllocationStatementsApi.Services/Implementations/IAllocationSearchService/Models/AllocationApiSearchAllocation.cs
+++ Pds-AllocationStatements-Api/AllocationStatementsApi.Services/Implementations/IAllocationSearchService/Models/AllocationApiSearchAllocation.cs
@@ -11,71 +11,71 @@ namespace AllocationStatementsApi.Services.Implementations.IAllocationSearchServ
         /// <summary>
         /// The UKPRN of the provider.
         /// </summary>
-        public required string Ukprn { get; set; }
+        public string Ukprn { get; set; }
 
         /// <summary>
         /// The type of provider.
         /// </summary>
-        public required string ProviderType { get; set; }
+        public string ProviderType { get; set; }
 
         /// <summary>
         /// The sub-type of provider.
         /// </summary>
-        public required string ProviderSubType { get; set; }
+        public string ProviderSubType { get; set; }
 
         /// <summary>
         /// The academic year of the allocation.
         /// </summary>
-        public required int Year { get; set; }
+        public int Year { get; set; }
 
         /// <summary>
         /// The version of this allocation.
         /// </summary>
-        public required int Version { get; set; }
+        public int Version { get; set; }
 
         /// <summary>
         /// The provider name.
         /// </summary>
-        public required string Name { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// The provider establishment number.
         /// </summary>
-        public required string EstablishmentNumber { get; set; }
+        public string EstablishmentNumber { get; set; }
 
         /// <summary>
         /// The LA code.
         /// </summary>
-        public required string LaCode { get; set; }
+        public string LaCode { get; set; }
 
         /// <summary>
         /// The LA name.
         /// </summary>
-        public required string LaName { get; set; }
+        public string LaName { get; set; }
 
         /// <summary>
         /// The provider postcode.
         /// </summary>
-        public required string Postcode { get; set; }
+        public string Postcode { get; set; }
 
         /// <summary>
         /// The provider town.
         /// </summary>
-        public required string Town { get; set; }
+        public string Town { get; set; }
 
         /// <summary>
         /// The date and time this allocation was published.
         /// </summary>
-        public required DateTime PublishedDateTime { get; set; }
+        public DateTime PublishedDateTime { get; set; }
 
         /// <summary>
         /// The total amount of this allocation.
         /// </summary>
-        public required decimal TotalAmount { get; set; }
+        public decimal TotalAmount { get; set; }
 
         /// <summary>
         /// A collection of funding streams for the provider.
         /// </summary>
-        public required IEnumerable<IFundingStream> Streams { get; set; }
+        public IEnumerable<IFundingStream> Streams { get; set; }
     }
 }
